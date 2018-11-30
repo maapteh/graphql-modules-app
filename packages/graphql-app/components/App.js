ee293363cb3b12821d119e0c18ec82e25731121d
Maapteh <maarten@oudenniel.nl>
2018-11-30 18:29:21 +0100

    feature(app): add viewport

--- packages/graphql-app/components/App.js
+++ packages/graphql-app/components/App.js
@@ -1,3 +1,12 @@
+import Head from 'next/head';
 import './app.scss';
 
-export default ({children}) => <main>{children}</main>;
+export default ({children}) => (
+    <div>
+        <Head>
+            <title>APOLLO GRAPHQL - flights</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <main>{children}</main>
+    </div>
+);
