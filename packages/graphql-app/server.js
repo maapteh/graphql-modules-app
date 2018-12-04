bb3c962dc162f619dbebe4323312c0c698b7aa0d
Maapteh <maarten@oudenniel.nl>
2018-12-04 13:58:33 +0100

    make full url as entries work using express

---
+++ packages/graphql-app/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const next = require('next');
+
+const port = parseInt(process.env.PORT, 10) || 4001;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare()
+  .then(() => {
+    const server = express();
+
+    server.get('/products', (req, res) => {
+      return app.render(req, res, '/products', req.query)
+    });
+
+    server.get('/products/:id', (req, res) => {
+        return app.render(req, res, '/products', { id: req.params.id })
+    });
+
+    server.get('/flight/:id', (req, res) => {
+      return app.render(req, res, '/flights', { id: req.params.id })
+    });
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    });
+
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    });
+
+});
