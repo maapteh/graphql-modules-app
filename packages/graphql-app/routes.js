ce1f8df11f72148541738ebcbd8405e3679f859e
Maapteh <maarten@oudenniel.nl>
2018-12-05 13:55:28 +0100

    remove routes, not working as i wanted

--- packages/graphql-app/routes.js
+++
@@ -1,7 +0,0 @@
-const routes = module.exports = require('next-routes')()
-
-routes
-    .add('about')
-    .add('/products', '/products/:id', 'products')
-    .add('/flights', '/flights/:id', 'flights')
-    .add({name: 'v1', pattern: '/v1', page: 'index'})
--- packages/graphql-app/server.js
+++ packages/graphql-app/server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const next = require('next');
-const routes = require('./routes');
 const dev = process.env.NODE_ENV !== 'production';
 const port = parseInt(process.env.PORT, 10) || 4001;
 const app = next({ dev });
-const handler = routes.getRequestHandler(app);
+const handler = app.getRequestHandler();
 
 app.prepare()
     .then(() => {
-        express().use(handler).listen(port, (err) => {
-            if (err) throw err
-            console.log(`> Ready on http://localhost:${port}`)
+        const server = express()
+
+        server.get('/products/:id', (req, res) => {
+            const actualPage = '/flight';
+            const queryParams = { id: req.params.id };
+            app.render(req, res, actualPage, queryParams);
         });
-});
+
+        server.get('*', (req, res) => {
+            return handler(req, res);
+        });
+
+        server.listen(port, (err) => {
+            if (err) throw err
+            console.log(`Ready on http://localhost:${port}`)
+        });
+    });
