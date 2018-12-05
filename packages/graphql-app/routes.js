06e1e52cdf6e454c4c80d534b74f216c2259c1a1
Maapteh <maarten@oudenniel.nl>
2018-12-05 12:02:03 +0100

    test heroku with “/“ (working locally but not on heroic when using route entry point differently then root

--- packages/graphql-app/routes.js
+++ packages/graphql-app/routes.js
@@ -2,6 +2,6 @@ const routes = module.exports = require('next-routes')()
 
 routes
     .add('about')
-    .add('products', '/products/:id')
-    .add('flights', '/flights/:id')
+    .add('/products', '/products/:id', 'products')
+    .add('/flights', '/flights/:id', 'flights')
     .add({name: 'v1', pattern: '/v1', page: 'index'})
