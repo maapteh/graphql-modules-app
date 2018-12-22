67e9d1b8e2c8b494089c4aab6c2999b277338c5c
Maapteh <maarten@oudenniel.nl>
2018-12-22 11:40:40 +0100

    http and https

--- packages/graphql-server/src/allowed-origins.ts
+++ packages/graphql-server/src/allowed-origins.ts
@@ -2,6 +2,9 @@ export const allowedOrigins = [
     'http://localhost:4000',
     'http://localhost:4001',
     'http://graphql-server-schiphol.herokuapp.com',
+    'https://graphql-server-schiphol.herokuapp.com',
+    'http://graphql-schiphol.herokuapp.com',
+    'https://graphql-schiphol.herokuapp.com',
     // @ts-ignore
     process.env.ALLOWED_ORIGIN,
 ];
