9564c5b8e53ccce56965000da2f42396a4879e15
maapteh <maarten@oudenniel.nl>
2019-09-05 18:14:21 +0200

    dummy

--- packages/graphql-server/src/app.ts
+++ packages/graphql-server/src/app.ts
@@ -1,9 +1,11 @@
 import { GraphQLModule } from '@graphql-modules/core';
+// TODO: Sample with dummy data, no keys needed
+import { contentModule } from './modules/content';
 // When you have an OpenApi key from Bol.com
 import { productModule } from './modules/product';
 import { commonModule, ApolloClientContext } from './modules/common';
 
 export const appModule = new GraphQLModule<{}, {}, ApolloClientContext>({
     /** a list of `GraphQLModule` you wish to load to your app */
-    imports: [productModule, commonModule],
+    imports: [contentModule, productModule, commonModule],
 });
