db6b14bd0af0fbf347a2b3f4718e5fd878ff4081
maapteh <maarten@oudenniel.nl>
2019-09-05 23:02:09 +0200

    upped dotenv

--- packages/graphql-server/src/index.ts
+++ packages/graphql-server/src/index.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 
 if (process.env.NODE_ENV === 'development') {
     dotenv.config({ path: `${__dirname}/../.env` });
-    dotenv.load();
 }
 
 import { bootstrap } from './server';
