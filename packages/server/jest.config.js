4954546adf9fb1f2fe2185232f0a5504233e84c8
maapteh <maarten@oudenniel.nl>
2019-09-10 14:34:19 +0200

    bugfix() because i also used mock dir which i moved to tst i had to set rootDir into ./ too

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -5,5 +5,12 @@ module.exports = {
     testRegex: '(/src/.*(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     modulePathIgnorePatterns: ['dist'],
-    collectCoverage: true,
+    collectCoverage: false,
+    globals: {
+        "ts-jest": {
+            "diagnostics": {
+                "warnOnly": true
+            }
+        }
+    }
 };
