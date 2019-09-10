df226728433bd17f791c818669db1f8c4b9631e9
maapteh <maarten@oudenniel.nl>
2019-09-11 00:27:43 +0200

    add two more test scenario's for single component

--- packages/app/src/components/product/elements/product-details/product-details.spec.tsx
+++ packages/app/src/components/product/elements/product-details/product-details.spec.tsx
@@ -5,20 +5,44 @@ import { DATA_9200000111963040 } from '../../../../../../server/test/__mocks__/s
 
 import { ProductDetails } from './product-details';
 
-const mock: any = { getProduct: {...DATA_9200000111963040} }; // Types of property '__typename' are incompatible
+const mock: any = { 
+    getProduct: {...DATA_9200000111963040}
+}; // Types of property '__typename' are incompatible
 
 describe('ProductDetails', () => {
-    it("shows correctly", async () => {
+    it("shows the default correctly", async () => {
 
         // findByTestId, queryByText, getByLabelText,
         const { container } = render(<ProductDetails data={mock} />);
-        // as any since __typename's are wrong
+
         mockAllIsIntersecting(true);
 
         // generic contract
         expect(container).toMatchSnapshot();
 
-        // specific expectations
+        // specific expectations 
+        // FIXME: add more
         expect(container.querySelector('h1').innerHTML).toBe('Ghost Recon Breakpoint Auroa Edition - PS4 (0)');
     });
+
+
+    it("shows short version correctly", async () => {
+
+        const { queryByText } = render(<ProductDetails data={mock} short/>);
+
+        mockAllIsIntersecting(true);
+
+        expect(queryByText(/features en gameplay-opties/)).toBeNull();
+        expect(queryByText('see @bol.com')).toBeNull()
+    });
+
+    it("shows no version", async () => {
+
+        const { container } = render(<ProductDetails data={null}/>);
+
+        mockAllIsIntersecting(true);
+
+        expect(container.innerHTML).toBe('');
+    });
+
 });
