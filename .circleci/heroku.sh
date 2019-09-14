0afa8dca9fbdf5999d7864e4d13bae419b500f0f
maapteh <info@maas38.nl>
2019-09-14 17:58:08 +0200

    make circleci work, run linting and testing

---
+++ .circleci/heroku.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -e
+
+usage() {
+  echo "OVERVIEW: Build apps according to BUILD_ENV value. Meant to be used for Heroku deployment"
+  exit
+}
+
+if [ "$1" = '-h' ] || [ "$1" = '--help' ]; then
+  usage
+fi
+
+(
+  PROJECT_ROOT="$(cd $(dirname $0)/..; pwd)"
+
+  cd $PROJECT_ROOT
+
+  if [ "$BUILD_ENV" = "app" ]; then
+    yarn workspace app build
+  elif [ "$BUILD_ENV" = "server" ]; then
+    yarn workspace server build
+  else
+    echo "Error: no build config for BUILD_ENV value '$BUILD_ENV'"
+    exit 1
+  fi
+)
