3aae66bbebd961c0c136050707e3cf1be79539e6
maapteh <maarten@oudenniel.nl>
2019-09-15 21:58:19 +0200

    remove sonar from server, using toolkit

--- packages/server/src/modules/product/index.ts
+++ packages/server/src/modules/product/index.ts
@@ -1,16 +1,12 @@
 import { GraphQLModule } from '@graphql-modules/core';
-import { loadSchemaFiles } from '@graphql-modules/sonar';
-import { mergeGraphQLSchemas } from '@graphql-modules/epoxy';
-import { IResolvers } from 'graphql-tools';
-
+import { loadResolversFiles, loadSchemaFiles } from 'graphql-toolkit';
 import { ProductProvider } from './providers/product';
 import { commonModule } from '../common';
-import resolvers from './resolvers/resolvers';
 
 export const productModule = new GraphQLModule({
     name: 'products',
     providers: [ProductProvider],
     imports: [commonModule],
-    typeDefs: mergeGraphQLSchemas(loadSchemaFiles(`${__dirname}/schema/`)),
-    resolvers: resolvers as IResolvers,
+    typeDefs: loadSchemaFiles(`${__dirname}/schema/`),
+    resolvers: loadResolversFiles(`${__dirname}/resolvers/`),
 });
--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # Setup default env for server
 if [ ! -e ./packages/server/.env ]; then
     echo "BOL_API_KEY=
-NODE_ENV=development
-MOCK_API=on" > "./packages/server/.env"
+NODE_ENV=\"development\"
+MOCK_API=\"on\"" > "./packages/server/.env"
     echo "⚡ Created a mock .env inside server packages, 
     turn mock off by placing your bol open api key and set MOCK_API to off"
 fi
